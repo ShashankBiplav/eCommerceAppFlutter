9698c516ab0f5e7f688742cbb5b5abc584160661
ShashankBiplav <Biplavshashank7@gmail.com>
2020-06-16 19:47:23 +0530

    Added optimistic updating for the favourite status

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Product with ChangeNotifier{
+class Product with ChangeNotifier {
   final String id;
   final String title;
   final String description;
@@ -16,8 +18,27 @@ class Product with ChangeNotifier{
     @required this.imageUrl,
     this.isFavourite = false,
   });
-  void toggleFavouriteStatus(){
+
+  void _setFavValue(bool newValue){
+      isFavourite = newValue;
+      notifyListeners();
+  }
+  // Apply optimistic updating for smooth UI performance
+  Future<void> toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = 'https://ecommerceappflutter-1feb8.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavourite': isFavourite}),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
